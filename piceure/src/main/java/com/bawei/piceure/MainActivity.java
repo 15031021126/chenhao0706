ba382d2306cff0e67be57eb41140aa99bb1b10ce
15031021126 <3309542361cc>
2019-07-06 16:08:15 +0800

    二次修改点击后弹出消失

--- piceure/src/main/java/com/bawei/piceure/MainActivity.java
+++ piceure/src/main/java/com/bawei/piceure/MainActivity.java
@@ -63,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         // 进入相册选择
+                        photoPopupWindow.dismiss();
                         PictureSelector.create(MainActivity.this)
                                 .openGallery(PictureMimeType.ofImage())//全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
                                 .maxSelectNum(1)// 最大图片选择数量 int
@@ -76,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         // 拍照
+                        // 进入相册选择
+                        photoPopupWindow.dismiss();
                         PictureSelector.create(MainActivity.this)
                                 .openCamera(PictureMimeType.ofImage())
                                 .forResult(1);
