8711bbc1887c31fc261e9afb1d7d02cb26e01ce2
15031021126 <3309542361cc>
2019-07-17 21:17:10 +0800

    事件拦截浏览器的demo

--- js/src/main/java/com/bawei/js/MySencondFragmentScrollView.java
+++ js/src/main/java/com/bawei/js/MySencondFragmentScrollView.java
@@ -5,6 +5,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
@@ -27,37 +28,41 @@ import android.widget.ScrollView;
     private ListView mListView;
     private float mLastY;//记录y轴坐标
 
+    /**
+     * 事件拦截
+     * @param ev
+     * @return
+     */
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        getChildListView();
+        getChildListView();//获取子布局
         int action = ev.getAction();
         switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                super.onInterceptTouchEvent(ev);
-                intercepet = false;
+            case MotionEvent.ACTION_DOWN://按下
+                super.onInterceptTouchEvent(ev);//不拦截
+                intercepet = false;//不拦截
                 break;
-            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_MOVE://移动
                 //满足listView滑动到顶部，如果继续下滑，那就让scrollView拦截事件
-                float y = ev.getY()-mLastY;
+                float y = ev.getY()-mLastY;//判断是否滑动
                 System.out.println("lastymove:"+y);
                 System.out.println("lastyevy:"+ev.getY());
                 if (mListView.getFirstVisiblePosition() ==0 && y>0) {//>0继续往下滑动
-                    intercepet = true;
+                    intercepet = true;//拦截
                 }
                 //满足listView滑动到底部，如果继续上滑，那就让scrollView拦截事件，《0时候继续往上滑
                 else if (mListView.getLastVisiblePosition() ==mListView.getCount()-1 && (ev.getY()-mLastY)<0) {
-                    intercepet = true;
+                    intercepet = true;//拦截
                 } else {
-                    intercepet = false;
+                    intercepet = false;//不拦截
                 }
                 break;
             case MotionEvent.ACTION_UP:
-                intercepet = false;
+                intercepet = false;//抬起不拦截
                 break;
         }
 
-
-        mLastY =ev.getY();
+        mLastY =ev.getY();//获取当前的Y轴坐标
         System.out.println("lasty："+mLastY);
         return intercepet;//动态返回，根据业务逻辑
     }
@@ -66,26 +71,19 @@ import android.widget.ScrollView;
      * 得到scrlloview的子view，listview
      */
     public void getChildListView() {
-//        View childAt = getChildAt(0);
-//        if (childAt instanceof ListView) {
-//            mListView = (ListView) childAt;
-//        } else {
-//            if (childAt instanceof ViewGroup) {
-//                ViewGroup viewGroup = (ViewGroup) childAt;
-//                for (int i = 0; i < viewGroup.getChildCount() - 1; i++) {
-//                    View childAt1 = viewGroup.getChildAt(i);
-//                    if(childAt1 instanceof  ListView){
-//                        mListView = (ListView)childAt1;
-//                    }
-//                }
-//            }
-//        }
-
-        View layout = getChildAt(0);
-
-        LinearLayout linearLayout = (LinearLayout) layout;
-
-        mListView = (ListView) linearLayout.getChildAt(1);
-
+        View childAt = getChildAt(0);//获得子布局
+        if (childAt instanceof ListView) {//判断是否为listview
+            mListView = (ListView) childAt;
+        } else {
+            if (childAt instanceof ViewGroup) {//断是否为listview
+                ViewGroup viewGroup = (ViewGroup) childAt;
+                for (int i = 0; i < viewGroup.getChildCount() - 1; i++) {
+                    View childAt1 = viewGroup.getChildAt(i);
+                    if(childAt1 instanceof  ListView){
+                        mListView = (ListView)childAt1;
+                    }
+                }
+            }
+        }
     }
 }
